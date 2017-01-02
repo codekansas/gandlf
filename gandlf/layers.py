cfb3996b3ed17f4d9cc8d9d13f20d8d3be9abce5
Benjamin Bolte <bkbolte18@gmail.com>
2017-01-02 18:06:41 -0500

    added permanent dropout layer :+1:

--- gandlf/layers.py
+++ gandlf/layers.py
@@ -364,3 +364,19 @@ class RecurrentAttention2D(keras.layers.Wrapper):
 
         base_config = super(RecurrentAttention1D, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
+
+
+class PermanentDropout(keras.layers.Dropout):
+    """Applies dropout to the input that isn't turned off during testing.
+
+    This is one possible improvement for your generator models.
+
+    Args:
+        p: float between 0 and 1. Fraction of the input units to drop.
+    """
+
+    def call(self, x, mask=None):
+        if 0. < self.p < 1.:
+            noise_shape = self._get_noise_shape(x)
+            x = K.dropout(x, self.p, noise_shape)
+        return x
